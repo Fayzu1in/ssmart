23f5730008422ae2f4e78725726dc9baef5a53a9
bugsssssss <n.soatov@bk.ru>
2023-05-22 16:36:09 +0500

    settings updt

--- backend/ssmart/settings.py
+++ backend/ssmart/settings.py
@@ -136,9 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'api/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-print(STATIC_ROOT)
 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
